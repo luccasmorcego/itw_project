df5d16bcc19e5eb538a40bbc7c79568f3bb15f40
Usuário <gamerlucks248@gmail.com>
2025-03-25 02:21:52 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById('mostrarMensagem').addEventListener('click', function() {
     document.getElementById('mensagem').style.display = 'block';
+    document.body.style.backgroundImage = "url('imagens/loira.jpeg')"; 
+    document.body.style.backgroundSize = "cover"; 
+    document.body.style.backgroundPosition = "center"; 
 });

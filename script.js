8b5c80a2fcfa028b8782be15b1a61cc58de0a6b8
Usuário <gamerlucks248@gmail.com>
2025-03-25 19:48:14 -0300

    UPDATE

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.getElementById('mostrarMensagem').addEventListener('click', function() {
     const mensagem = document.getElementById('mensagem');
+    const botalink = document.getElementById('btn');
     const botao = document.getElementById('mostrarMensagem');
     
     if (mensagem.style.display === 'none' || !mensagem.style.display) {
         mensagem.style.display = 'block';
+        botalink.style.display = 'block'
         document.body.style.backgroundImage = "url('imagens/loira.jpeg')"; 
         document.body.style.backgroundSize = "cover"; 
         document.body.style.backgroundPosition = "center"; 
@@ -16,4 +18,8 @@ document.getElementById('mostrarMensagem').addEventListener('click', function() 
         // Alterar o texto do botão de volta
         botao.innerText = 'Mostrar Mensagem';
     }
+
+
 });
+
+
